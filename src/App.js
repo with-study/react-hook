259e9fad9a73aeac6a4abb869f442940ebaf54db
유진철Wheely(윌리) <wheely@within.co.kr>
2019-08-22 17:30:10 +0900

    테스트 190822

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import SignUp from './pages/signUp';
 
 const App = () => {
-  return (
-    <div>
-   <SignUp />   
-    </div>
-  )
+	return (
+		<div>
+			<SignUp />   
+		</div>
+	)
 }
 
 export default App;
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -6,11 +6,6 @@ const signUp = () => {
             <p><input type="text" className={`input-default`} placeholder="아이디입력"/></p>
             <p><input type="password"className={`input-default`} placeholder="비밀번호입력"/></p>
             <p><button className={`button-default`} type="button">회원가입</button></p>
-
-
-
-
-            
         </div>
     )
 }
